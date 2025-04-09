547dd98f2bbf8abaff8f9a91b4ffbacc7c60cbf3
kayes360 <42603790+kayes360@users.noreply.github.com>
2025-04-10 05:58:42 +0600

    redeploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,34 @@
+
+
 import AppSidebar from "@/components/AppSidebar"; 
 import ProductGrid from "@/components/ProductGrid"; 
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { products as productList } from "@/data/products"; // ✅ Direct import — no fetch needed
+import { IProduct } from "@/types/product-type";
 
-export default function Home() {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <div className="w-full p-5">
-        <SidebarTrigger className="z-1 relative cursor-pointer" />
-        <ProductGrid productList={productList} />
-      </div>
-    </SidebarProvider>
-  );
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+export default async function Home() {
+  try {
+    const res = await fetch(`${API_URL}/api/products`, {  
+        cache: 'force-cache' 
+      });
+    if (!res.ok) {
+      throw new Error("Failed to fetch product list");
+    }
+
+    const productList: IProduct[] = await res.json();
+
+    return (
+      <>
+        <SidebarProvider>
+          <AppSidebar />
+          <div className="w-full p-5">
+            <SidebarTrigger className="z-1 relative cursor-pointer" />
+            <ProductGrid productList={productList} />
+          </div>
+        </SidebarProvider>
+      </>
+    );
+  } catch (err) { 
+    throw err;
+  }
 }
