7c24306c8d74b9dc7756fc0d25519a1983be865c
kayes360 <42603790+kayes360@users.noreply.github.com>
2025-04-10 05:56:50 +0600

    mock

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,16 @@
 import AppSidebar from "@/components/AppSidebar"; 
 import ProductGrid from "@/components/ProductGrid"; 
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { IProduct } from "@/types/product-type";
+import { products as productList } from "@/data/products"; // ✅ Direct import — no fetch needed
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
-export default async function Home() {
-  try {
-    const res = await fetch(`${API_URL}/api/products`, {  
-        cache: 'force-cache' 
-      });
-    if (!res.ok) {
-      throw new Error("Failed to fetch product list");
-    }
-
-    const productList: IProduct[] = await res.json();
-
-    return (
-      <>
-        <SidebarProvider>
-          <AppSidebar />
-          <div className="w-full p-5">
-            <SidebarTrigger className="z-1 relative cursor-pointer" />
-            <ProductGrid productList={productList} />
-          </div>
-        </SidebarProvider>
-      </>
-    );
-  } catch (err) { 
-    throw err;
-  }
+export default function Home() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <div className="w-full p-5">
+        <SidebarTrigger className="z-1 relative cursor-pointer" />
+        <ProductGrid productList={productList} />
+      </div>
+    </SidebarProvider>
+  );
 }
